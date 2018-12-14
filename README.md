4765ac5ce45a22e71f9e36296f739ab2dbd6fd5f
Niranjan Andral <andral.niranjan@gmail.com>
2018-12-14 14:42:35 +1100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# advanced-react-redux-testing
-Created with CodeSandbox
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CommentBox from "components/CommentBox";
+import CommentsList from "components/CommentsList";
+const App = () => (
+  <div>
+    <CommentBox />
+    <CommentsList />
+  </div>
+);
+
+export default App;
